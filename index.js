0430210ca0c6f1464ce337e00f2748ff9c49cc93
Trip Talley <ttalley00@yahoo.com>
2018-03-12 13:46:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ function shout(string) {
     
   }
   
-  else if (string.toUpperCase() === string) {return string.toLowerCase ("Yes Indeed!")
+  else if (string.toUpperCase() === string) {return string.toLowerCase ("YES INDEED!")}
   
